b6644a43cadf5b594680f4d7c4876d86ffb0f18c
Jack Rhoa <jackrhoa1@users.noreply.github.com>
2025-05-23 18:27:01 -0400

    added name via join

--- DjangoAPI/baseball_stats/app/serializers.py
+++ DjangoAPI/baseball_stats/app/serializers.py
@@ -2,22 +2,28 @@ from rest_framework import serializers
 from .models import BatterStat, PitcherStat, PlayerInfo, FieldingStat, GameInfo
 
 class BatterStatSerializer(serializers.ModelSerializer):
+    player_name = serializers.CharField(source='player_id.player_name', read_only=True)
+
     class Meta:
         model = BatterStat
         fields = '__all__'
-        read_only_fields = ['id']
+        read_only_fields = ['id'] + ['player_name']
 
 class PitcherStatSerializer(serializers.ModelSerializer):
+    player_name = serializers.CharField(source='player_id.player_name', read_only=True)
+
     class Meta:
         model = PitcherStat
         fields = '__all__'
-        read_only_fields = ['id']
+        read_only_fields = ['id'] + ['player_name']
 
 class FieldingStatSerializer(serializers.ModelSerializer):
+    player_name = serializers.CharField(source='player_id.player_name', read_only=True)
+    
     class Meta:
         model = FieldingStat
         fields = '__all__'
-        read_only_fields = ['id']
+        read_only_fields = ['id'] + ['player_name']
 
 class GameInfoSerializer(serializers.ModelSerializer):
     class Meta:
